17a036b1083de4441bc49c516709e43db52567fc
kihooncode <kihooncode@gmail.com>
2021-01-25 23:44:49 +0900

    LET'S DO THIS!

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import {
+   StyleSheet,
+   View
+} from 'react-native';
+
+export default function App() {
+   return (
+      <View>
+   
+      </View>
+   );
+}
+
+const styles = StyleSheet.create({
+});
