53b307fd6f186c104e094de5a47695d10f75d902
kihooncode <kihooncode@gmail.com>
2021-02-04 23:24:36 +0900

    Just added fonts styling to Start Screen title :) well done 1 commit a day :)

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+const fetchFonts = () => {
+   return Font.loadAsync({
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+   });
+}
 
 export default function App() {
    // user's final guess
    const [userGuess, setUserGuess] = useState();
    // number of guesses taken to get user's number
    const [numGuess, setNumGuess] = useState(0);
+   // Whether all data is loaded for app to function
+   const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+   if (!isDataLoaded) {
+      return <AppLoading
+         startAsync={fetchFonts}
+         onFinish={() => setIsDataLoaded(true)}
+         onError={err => console.log(err)}
+      />
+   }
 
    /**
     * Starts the game with the final guess.
